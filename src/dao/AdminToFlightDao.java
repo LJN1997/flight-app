2f1c2efb0133f0958e3cdcfaafee59f278f814cf
LJN <1677001686@qq.com>
2020-01-01 12:33:29 +0800

    航空项目搭建

--- src/dao/AdminToFlightDao.java
+++ src/dao/AdminToFlightDao.java
@@ -5,13 +5,5 @@ import entity.Flight;
 import java.util.List;
 
 public interface AdminToFlightDao {
-    //增加航班
-    public int insert();
-    //删除航班
-    public int delete();
-    //查询航班
-    public List<Flight> find();
-    //修改航班
-    public int updateFlight();
 
 }
